48b9d99f470b3d077e6205fa87fcf0d5ea464321
radhe@kad513 <shwetakad513@gmail.com>
2023-12-01 12:21:15 +0530

    Wise Product Display In E Commerce Website

--- ecopro/shop/views.py
+++ ecopro/shop/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products=product.objects.all()
+    print(products)
+    # n=len(products)
+    # nslides=n//4 + ceil((n/4)-(n//4))
+
+    allprods=[]
+    catprods=product.objects.values('category','id')
+    cats={item['category'] for item in catprods}
+    for cat in cats:
+        prod=product.objects.filter(category=cat)
+        n=len(prod)
+        nslides=n//4 + ceil((n/4)-(n//4))
+        allprods.append([prod,range(1,nslides),nslides])
+
+    # params={'no_of_slide':nslide,'range':range(1,nslide),'product':products}
+    # allprods=[[products,range(1,nslides),nslides],
+    #           [products,range(1,nslides),nslides]]
+    params={'allprods':allprods}
+    return render(request,'shop/index.html',params)
 
 def about(request):
     return render(request,'shop/about.html')
