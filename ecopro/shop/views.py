0472bd519cdca8a1710aa922525c43aa5614f21f
radhe@kad513 <shwetakad513@gmail.com>
2023-12-06 12:47:50 +0530

    Entry for orders

--- ecopro/shop/views.py
+++ ecopro/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import product,Contact
+from .models import product,Contact,Orders
+# from .models import product,Contact
 from math import ceil
 
 # Create your views here.
@@ -50,4 +51,21 @@ def productview(request,myid):
     return render(request,'shop/productview.html',{'product':products[0]})
 
 def checkout(request):
-    return render(request,"shop/checkout.html")
+    print("checkout")
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
+    return render(request, 'shop/checkout.html')
